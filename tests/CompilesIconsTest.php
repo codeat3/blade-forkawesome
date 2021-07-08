7168d2217a53869a1530006ce8e69ffc6b9c3543
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-07-08 21:10:43 +0530

    wip

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -18,7 +18,7 @@ class CompilesIconsTest extends TestCase
 
         // Note: the empty class here seems to be a Blade components bug.
         $expected = <<<'SVG'
-            <svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path d="M768 1440V352c-300 0-544 244-544 544s244 544 544 544zm768-544c0 424-344 768-768 768S0 1320 0 896s344-768 768-768 768 344 768 768z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1792 1792" fill="currentColor"><path d="M768 1440V352c-300 0-544 244-544 544s244 544 544 544zm768-544c0 424-344 768-768 768S0 1320 0 896s344-768 768-768 768 344 768 768z"/></svg>
             SVG;
 
         $this->assertSame($expected, $result);
@@ -30,7 +30,7 @@ class CompilesIconsTest extends TestCase
         $result = svg('forkawesome-adjust', 'w-6 h-6 text-gray-500')->toHtml();
 
         $expected = <<<'SVG'
-            <svg class="w-6 h-6 text-gray-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path d="M768 1440V352c-300 0-544 244-544 544s244 544 544 544zm768-544c0 424-344 768-768 768S0 1320 0 896s344-768 768-768 768 344 768 768z"/></svg>
+            <svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1792 1792" fill="currentColor"><path d="M768 1440V352c-300 0-544 244-544 544s244 544 544 544zm768-544c0 424-344 768-768 768S0 1320 0 896s344-768 768-768 768 344 768 768z"/></svg>
             SVG;
 
         $this->assertSame($expected, $result);
@@ -42,7 +42,7 @@ class CompilesIconsTest extends TestCase
         $result = svg('forkawesome-adjust', ['style' => 'color: #555'])->toHtml();
 
         $expected = <<<'SVG'
-            <svg style="color: #555" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path d="M768 1440V352c-300 0-544 244-544 544s244 544 544 544zm768-544c0 424-344 768-768 768S0 1320 0 896s344-768 768-768 768 344 768 768z"/></svg>
+            <svg style="color: #555" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1792 1792" fill="currentColor"><path d="M768 1440V352c-300 0-544 244-544 544s244 544 544 544zm768-544c0 424-344 768-768 768S0 1320 0 896s344-768 768-768 768 344 768 768z"/></svg>
             SVG;
 
         $this->assertSame($expected, $result);
@@ -56,7 +56,7 @@ class CompilesIconsTest extends TestCase
         $result = svg('forkawesome-adjust')->toHtml();
 
         $expected = <<<'SVG'
-            <svg class="awesome" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path d="M768 1440V352c-300 0-544 244-544 544s244 544 544 544zm768-544c0 424-344 768-768 768S0 1320 0 896s344-768 768-768 768 344 768 768z"/></svg>
+            <svg class="awesome" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1792 1792" fill="currentColor"><path d="M768 1440V352c-300 0-544 244-544 544s244 544 544 544zm768-544c0 424-344 768-768 768S0 1320 0 896s344-768 768-768 768 344 768 768z"/></svg>
             SVG;
 
         $this->assertSame($expected, $result);
@@ -71,7 +71,7 @@ class CompilesIconsTest extends TestCase
         $result = svg('forkawesome-adjust', 'w-6 h-6')->toHtml();
 
         $expected = <<<'SVG'
-            <svg class="awesome w-6 h-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path d="M768 1440V352c-300 0-544 244-544 544s244 544 544 544zm768-544c0 424-344 768-768 768S0 1320 0 896s344-768 768-768 768 344 768 768z"/></svg>
+            <svg class="awesome w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1792 1792" fill="currentColor"><path d="M768 1440V352c-300 0-544 244-544 544s244 544 544 544zm768-544c0 424-344 768-768 768S0 1320 0 896s344-768 768-768 768 344 768 768z"/></svg>
             SVG;
 
         $this->assertSame($expected, $result);
