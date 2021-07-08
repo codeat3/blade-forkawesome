fe53ea51c610d7cd69367e74a3f57d24afd39495
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2021-07-08 15:41:33 +0000

    Fix styling

--- src/BladeForkAwesomeServiceProvider.php
+++ src/BladeForkAwesomeServiceProvider.php
@@ -19,7 +19,6 @@ final class BladeForkAwesomeServiceProvider extends ServiceProvider
 
             $factory->add('forkawesome', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
         });
-
     }
 
     private function registerConfig(): void
@@ -39,5 +38,4 @@ final class BladeForkAwesomeServiceProvider extends ServiceProvider
             ], 'blade-forkawesome-config');
         }
     }
-
 }
